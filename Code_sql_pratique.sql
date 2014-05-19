5534481d685a487fc541f69965a66cef48496e71
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-05-19 18:31:45 -0400

    ajouter .sql pour maintenance BD

---
+++ Code_sql_pratique.sql
@@ -0,0 +1,15 @@
+SELECT * FROM catalogue;-- WHERE genre = 'Potion';
+
+update catalogue set poids = 12 WHERE genre = 'Armure';
+
+
+delete inventairejoueur where nomusager = 'Test';
+ SELECT * FROM panier;
+ SELECT * FROM inventairejoueur;
+ 
+commit;
+
+Select * from Potions ;
+delete inventairejoueur where numitem = 2 and nomusager= 'ariekor';
+
+delete catalogue where numitem = 13;
